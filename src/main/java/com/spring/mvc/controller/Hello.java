dfca26e134f615c5e9dcd74928c32d9604492272
teacher <teacher@192.168.1.180>
2020-12-07 20:02:39 +0800

    SSH

--- src/main/java/com/spring/mvc/controller/Hello.java
+++ src/main/java/com/spring/mvc/controller/Hello.java
@@ -2,10 +2,14 @@ package com.spring.mvc.controller;
 
 import java.util.Date;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Hello {
     
+    @RequestMapping("/time")
+    @ResponseBody
     public String time() {
         return new Date().toString();
     }
