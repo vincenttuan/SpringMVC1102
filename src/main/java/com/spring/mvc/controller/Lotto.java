ccdce1d4b4d26facb7da2c9b919e0c6a3ea48504
teacher <teacher@192.168.1.180>
2020-12-07 21:24:55 +0800

    SSH

--- src/main/java/com/spring/mvc/controller/Lotto.java
+++ src/main/java/com/spring/mvc/controller/Lotto.java
@@ -1,14 +1,23 @@
 package com.spring.mvc.controller;
 
+import com.spring.mvc.service.LottoService;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/lotto")
 public class Lotto {
     
+    @Autowired
+    private LottoService lottoService;
+    
     @RequestMapping("/get")
+    @ResponseBody
     public String get() {
-        
+        Set<Integer> lotto = lottoService.getLotto();
+        return lotto.toString();
     }
 }
