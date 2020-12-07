e80fa0fd69378734958ce1b4d701b73d6f27cd30
teacher <teacher@192.168.1.180>
2020-12-07 20:06:02 +0800

    SSH

--- src/main/java/com/spring/mvc/controller/Hello.java
+++ src/main/java/com/spring/mvc/controller/Hello.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/hello")
 public class Hello {
     
     @RequestMapping("/time")
