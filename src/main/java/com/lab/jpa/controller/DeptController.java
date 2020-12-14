d09a0e3d64cfd675424a2cb60ffa2eab3b4f5784
teacher <teacher@192.168.1.180>
2020-12-14 19:54:18 +0800

    SSH

--- src/main/java/com/lab/jpa/controller/DeptController.java
+++ src/main/java/com/lab/jpa/controller/DeptController.java
@@ -1,6 +1,10 @@
 package com.lab.jpa.controller;
 
+import com.lab.jpa.repository.CompanyDao;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/dept")
 public class DeptController {
     
+    @Autowired
+    private CompanyDao dao;
+    
     @RequestMapping(value = {"/"})
-    public String read() {
+    public String read(Model model) {
+        List dept_list = dao.queryAllDepts();
+        model.addAttribute("dept_list", dept_list);
         return "dept_page";
     }
     
