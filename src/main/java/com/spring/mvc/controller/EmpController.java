1162df5a8d1ace902ef6c4d132892f698da89ebd
teacher <teacher@192.168.1.180>
2020-12-14 19:19:02 +0800

    SSH

---
+++ src/main/java/com/spring/mvc/controller/EmpController.java
@@ -0,0 +1,16 @@
+package com.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/emp")
+public class EmpController {
+    
+    @RequestMapping(value = {"/"})
+    public String read() {
+        return "emp_page";
+    }
+    
+}
