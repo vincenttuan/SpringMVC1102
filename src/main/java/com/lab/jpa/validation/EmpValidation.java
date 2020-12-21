acf5a019634696959bff2eb6131edc01929eeca8
teacher <teacher@192.168.1.180>
2020-12-21 21:27:03 +0800

    SSH

---
+++ src/main/java/com/lab/jpa/validation/EmpValidation.java
@@ -0,0 +1,29 @@
+package com.lab.jpa.validation;
+
+import com.lab.jpa.entities.Department;
+import com.lab.jpa.entities.Employee;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class EmpValidation implements Validator{
+
+    @Override
+    public boolean supports(Class<?> type) {
+        return Employee.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Employee emp = (Employee)o;
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "emp.name.empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "salary.money", "salary.empty.empty");
+        if(emp.getSalary().getMoney() != null && emp.getSalary().getMoney() < 40000) {
+            errors.rejectValue("salary.money", "salary.empty.notenough");
+        }
+        
+    }
+    
+}
